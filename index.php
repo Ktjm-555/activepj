e971babfd67e0fd78a3bb078e75a9bc6fdf0e087
“Ktjm-555” <m250919kaho@gmail.com>
2022-05-02 23:46:38 +0900

    ボタン作成

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sanitaize.css">
+    <link rel="stylesheet" href="style.css">
+    <title>active_record</title>
+</head>
+<body>
+    <div class="main">
+        <div class="button-area">
+            <div>
+                <form action="" method="post" >
+                    <button type="submit"><img src="image/image01.png" alt="">ボタン</button>
+                </form>
+            </div>
+            <div>
+                <form action="" method="post" >
+                    <button type="submit"><img src="image/image02.png" alt="">ボタン</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
