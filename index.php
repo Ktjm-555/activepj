be0e8e4b5907264129e5d79fbd69736f4752ddf2
“Ktjm-555” <m250919kaho@gmail.com>
2022-05-17 11:27:30 +0900

    完成

--- index.php
+++ index.php
@@ -9,45 +9,74 @@
     <title>active_record</title>
 </head>
 <body>
-    <div class="main">
-        <div class="button-cnt">
-            <div class="active-area">
-                <input type="image" id="active" alt="active" src="image/image01.png" onclick="counting()">
-                <div class="active-display">
-                    <span id="active-cnt">0</span>回
-                </div>
-                <div class="level">
-                    <span id="level">初心者</span>
-                </div>
-            </div>
-            <div class="rest-area">   
-                <input type="image" id="rest" alt="rest" src="image/image02.png" onclick="counter()"> 
-                <div class="rest-display">
-                    <span id="rest-cnt">0</span>回 
-                </div>       
-            </div>             
+  <div class="main">
+    <div class="button-cnt">
+      <div class="active-area">
+        <input type="image" id="active" alt="active" src="image/image01.png" onclick="counting()">
+        <div class="active-display">
+          <span id="active-cnt">0</span>回
         </div>
+        <div class="level">
+          <span id="level">初心者</span>
         </div>
+      </div>
+      <div class="rest-area">   
+        <input type="image" id="rest" alt="rest" src="image/image02.png" onclick="counter()"> 
+        <div class="rest-display">
+          <span id="rest-cnt">0</span>回
+        </div>       
+        <div class="level_r">
+          <span id="level_r">今日くらいゆっくり</span>
+				</div>
+      </div>             
     </div>
+  </div>
+	<div class="reset">
+		<input type="button" value="リセット" id="reset" onclick="reset_do()">
+	</div>
+	<div class="cheer">
+		<img src="image/image03.png" id="cheer" onclick="alert('ここは押すもんじゃありません')">
+	</div>
     <script>
-        var active = 0;
-        function counting(){
-            active = active + 1;
-            document.getElementById("active-cnt").innerHTML = active;
-            if (active > 5){
-                document.getElementById("level").innerHTML = "成長期";
-                document.getElementById("level").innerHTML = "成長期";
+			window.onbeforeunload = function(reload) {
+    		reload.returnValue = "カウントが消えてしまいますよ";
+			}
 
-                // var img;
-                // img = document.createElement('img');
-                // img.setAttribute('src', 'image/image03.png');
-            }
-        }
-        var rest = 0;
-        function counter(){
-            rest++;
-            document.getElementById("rest-cnt").innerHTML = rest;
-        }
+			var active = 0;
+			function counting(){
+				active = active + 1;
+				document.getElementById("active-cnt").innerHTML = active;
+				if (active > 5){
+					document.getElementById("level").innerHTML = "成長期";
+					document.getElementById('active').src='image/active5.png';
+				}
+				if (active > 10){
+					document.getElementById("level").innerHTML = "継続の天才";
+					document.getElementById('active').src='image/active10.png';
+				}
+			}
+			var rest = 0;
+			function counter(){
+				rest++;
+				document.getElementById("rest-cnt").innerHTML = rest;
+				if (rest > 5){
+					document.getElementById("level_r").innerHTML = "適度にお休み";
+					document.getElementById('rest').src='image/rest5.png';
+				}
+				if (rest > 10){
+					document.getElementById("level_r").innerHTML = "ちょっと怠け者";
+					document.getElementById('rest').src='image/rest10.png';
+				}
+			}
+
+			function reset_do(){
+				document.getElementById("active-cnt").innerHTML = 0;
+				document.getElementById("rest-cnt").innerHTML = 0;
+				document.getElementById("level").innerHTML = "回帰初心者";
+				document.getElementById("level_r").innerHTML = "今日くらいゆっくりする？";
+			}
+
+			
 
     </script>
 </body>
